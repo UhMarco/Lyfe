225dbe97c09cc0e18a35d5d2d942e47dd53b7dc4
UhMarco <ddefygravity@gmail.com>
2020-07-16 18:39:46 +0100

    i should probably have made sure i was finished before committing

--- items/inventories.py
+++ items/inventories.py
@@ -58,6 +58,7 @@ class Inventories(commands.Cog):
                 await self.bot.inventories.upsert({"_id": ctx.author.id, "banklimit": 0})
                 await self.bot.inventories.upsert({"_id": ctx.author.id, "job": None})
                 await self.bot.inventories.upsert({"_id": ctx.author.id, "inventory": data})
+                page = 1
             else:
                 return await ctx.send(f"**{user.name}** hasn't initialized their inventory yet.")
 
