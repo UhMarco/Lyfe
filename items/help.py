67a306edfbe03dd542a0ed32ba5bc5b22c8aa3bb
UhMarco <ddefygravity@gmail.com>
2020-07-18 21:33:37 +0100

    Added gambling to help

--- items/help.py
+++ items/help.py
@@ -71,8 +71,9 @@ class Help(commands.Cog):
             return await ctx.send(embed=embed)
 
         elif section.lower() == "misc":
-            embed = discord.Embed(title="<:reddragon:733766679036952646> Misc Commands", color=discord.Color.purple())
+            embed = discord.Embed(title=":game_die: Misc Commands", color=discord.Color.purple())
             embed.set_footer(text="misc command list")
+            embed.add_field(name=f"`{self.bot.prefix}gamble`", value="See ways of spending money sensibly", inline=False)
             embed.add_field(name=f"`{self.bot.prefix}feed`", value="Feed a frog to a dragon, there's a 5% chance of something special happening", inline=False)
             embed.add_field(name=f"`{self.bot.prefix}avatar [user]`", value="Display the avatar of a user in full size", inline=False)
             embed.add_field(name=f"`{self.bot.prefix}8ball (question)`", value="8ball lol", inline=False)
@@ -116,7 +117,7 @@ class Help(commands.Cog):
         embed.add_field(name=":card_box: Jobs", value=f"`{self.bot.prefix}help jobs`", inline=False)
         embed.add_field(name=":scales: Trading", value=f"`{self.bot.prefix}help trading`", inline=False)
         embed.add_field(name=":moneybag: Robbery", value=f"`{self.bot.prefix}help robbery`", inline=False)
-        embed.add_field(name="<:reddragon:733766679036952646> Misc", value=f"`{self.bot.prefix}help misc`", inline=False)
+        embed.add_field(name=":game_die: Misc", value=f"`{self.bot.prefix}help misc`", inline=False)
         embed.add_field(name=":robot: Bot", value=f"`{self.bot.prefix}help bot`", inline=False)
         return await ctx.send(embed=embed)
 
