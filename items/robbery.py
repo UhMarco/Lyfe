46d7be798dbcc2e6b9910f3b8079e8fcc02dd383
UhMarco <ddefygravity@gmail.com>
2020-07-20 12:27:52 +0100

    Added find

--- items/robbery.py
+++ items/robbery.py
@@ -5,6 +5,7 @@ from pathlib import Path
 cwd = Path(__file__).parents[1]
 cwd = str(cwd)
 import utils.json
+from tabulate import tabulate
 
 robberytools = ['gun', 'hammer', 'knife']
 
@@ -216,6 +217,63 @@ class Robbery(commands.Cog):
         elif isinstance(error, commands.BadArgument):
             return await ctx.send("I couldn't find that user.")
 
+    # --------------------------------------------------------------------------
+    # ----- COMMAND: -----------------------------------------------------------
+    # ----- FIND ---------------------------------------------------------------
+    # --------------------------------------------------------------------------
+
+    @commands.command()
+    async def find(self, ctx, *, item):
+        data = await self.bot.inventories.get_all()
+        first, second, third, fourth, fifth = {}, {}, {}, {}, {}
+        all = {}
+        items = await self.bot.items.find("items")
+        items = items["items"]
+        item = item.replace(" ", "").lower()
+        if item not in items:
+            return await ctx.send("That item does not exist.\n**Tip:** Items in commands generally don't contain spaces!")
+        item = items[item]
+        name, emoji = item["name"], item["emoji"]
+
+        for item in data:
+            for i in item["inventory"]:
+                if i["name"] == name:
+                    all[item["_id"]] = i["quantity"]
+
+        all = sorted(all.items(), key=lambda k: k[1], reverse=True)
+        entries = []
+        count = 0
+        while True:
+            try:
+                rand = random.choice(all)
+            except IndexError:
+                entries.append(["None", 0, 0])
+                break
+            user = self.bot.get_user(int(rand[0]))
+            if user != None:
+                found = False
+                for i in entries:
+                    if i[2] == user.id:
+                        found = True
+                        count -= 1
+
+                if not found:
+                    entries.append([user, rand[1], user.id])
+            else:
+                count -= 1
+            count += 1
+
+            if count >= len(all) or count > 4:
+                break
+
+        output = ("```" + tabulate(entries, tablefmt="simple", headers=["Player", "Amount", "User ID"]) + "```")
+        embed = discord.Embed(title=f"{emoji} Owners of {name}s:", description=output, color=discord.Color.gold())
+        await ctx.send(embed=embed)
+
+    @find.error
+    async def find_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(f"Usage: `{self.bot.prefix}find (item)`")
 
 def setup(bot):
     bot.add_cog(Robbery(bot))
